5a2fcef0de994bf7cea02dac6fe139615bf5803e
amitkushwahatech <amitkushwahatech@gmail.com>
2025-02-05 21:39:25 +0530

    cros orin update for frontend access 2nd

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173','https://mern-auth-frontend-five.vercel.app/']
+const allowedOrigins = ['http://localhost:5173','https://mern-auth-frontend-five.vercel.app']
 
 app.use(express.json());
 app.use(cookieParser());
